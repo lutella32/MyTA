256b013f005518e2b78c6cf405f913bfd61b5467
ltella <lucie.tella@etu.univ-poitiers.fr>
2021-09-21 11:59:12 +0200

    if

--- app/src/main/java/com/example/myta/MainActivity.java
+++ app/src/main/java/com/example/myta/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.myta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) {
+        final EditText edit1= (EditText) findViewById(R.id.textinput);
+      // EditText text = (EditText) edit1.getText();
+      // String n=text.toString();
+     //  if(n.equals("")){
+            Log.d("start","h");
+            Context context = getApplicationContext();
+            CharSequence textToast = "You need to enter name";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, textToast, duration);
+            toast.show();
+       // }
+     // else{
         setContentView(R.layout.activity_starting);
+    //}
     }
+
 }
