014590c35acc13c8ae9601551a700299f20a52b2
ltella <lucie.tella@etu.univ-poitiers.fr>
2021-09-23 10:04:01 +0200

    5.1

--- app/src/main/java/com/example/myta/Results.java
+++ app/src/main/java/com/example/myta/Results.java
@@ -2,7 +2,10 @@ package com.example.myta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Results extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+
+    public void site(View view) {
+        String url = "https://fedecardio.org/je-me-teste/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
