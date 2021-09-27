7377f7417987e09ad290c1bceb12dd06ff2c3584
ltella <lucie.tella@etu.univ-poitiers.fr>
2021-09-27 19:23:28 +0200

    resultif

--- app/src/main/java/com/example/myta/Results.java
+++ app/src/main/java/com/example/myta/Results.java
@@ -7,9 +7,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
     Person person;
+    public enum Age{LessThan40, between40and60, more60};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,32 @@ public class Results extends AppCompatActivity {
             if (transferredPerson != null) {
                 this.person = transferredPerson;
                 this.person.print();
+                //if(person.getAge().equals("more60")) {
+                if (false == person.getCheckpoint() | false == person.getDoctor() | false ==person.getAppointment()) {
+
+                        TextView text1 = findViewById(R.id.textView12);
+                        text1.setText("you need to see a medecin to check your heart");
+                    if (true == person.getDiab() | true == person.getCardiacDisease() | true == person.getHypertension() | true == person.getCholestPb() ) {
+
+                        TextView text2 = findViewById(R.id.textView13);
+                        text2.setText("you need to see a keep a healthy life style");
+
+                    }
+                    }
+                else {
+                    if (true == person.getDiab() | true == person.getCardiacDisease() | true == person.getHypertension() | true == person.getCholestPb()) {
+
+                        TextView text2 = findViewById(R.id.textView12);
+                        text2.setText("you need to see a keep a healthy life style");
+
+                    } else {
+
+                        TextView text2 = findViewById(R.id.textView13);
+                        text2.setText("you are good");
+
+                    }
+                }
+               // }
             }
             else {
                 Log.d("Starting", "No Person found after transfer from Activity1");
