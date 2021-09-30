032d3eaa5433c413ec3f6f513642a483335edc9f
RACHEL <rachelvillard74@gmail.com>
2021-09-30 15:14:50 +0200

    correction mais number toujours pas ok

--- app/src/main/java/com/example/myta/Results.java
+++ app/src/main/java/com/example/myta/Results.java
@@ -11,29 +11,40 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
+
     Person person;
-    public enum Age{LessThan40, between40and60, more60};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // processIntentData();
         setContentView(R.layout.activity_results);
+
         Intent intent = getIntent();
         if(intent != null) {
             Person transferredPerson = intent.getParcelableExtra("FromEnvtToResult");
             if (transferredPerson != null) {
                 this.person = transferredPerson;
                 this.person.print();
-                if (person.getAge().equals(Person.Age.more60) | person.getAge().equals(Person.Age.between40and60)) {
+
+                TextView name = findViewById(R.id.textView22);
+                name.setText(person.getName());
+
+                TextView CD = findViewById(R.id.textView12);
+                if (person.getCardiacDisease()==true){ CD.setText("Cardiac disease : yes"); }
+                if (person.getCardiacDisease()==false){ CD.setText("Cardiac disease : no"); }
+
+                TextView Ch = findViewById(R.id.textView23);
+                if (person.getCardiacDisease()==true){ Ch.setText("Cholesterol problem : yes"); }
+                if (person.getCardiacDisease()==false){ Ch.setText("Cholesterol problem : no"); }
+
+               /* if (person.getAge().equals(Person.Age.more60) | person.getAge().equals(Person.Age.between40and60)) {
                         if (false == person.getCheckpoint() | false == person.getDoctor() | false == person.getAppointment()) {
-
                             TextView text1 = findViewById(R.id.textView12);
-                            text1.setText("you need to see a medecin to check your heart");
+                            text1.setText("You need to see a doctor to check your heart");
+
                             if (true == person.getDiab() | true == person.getCardiacDisease() | true == person.getHypertension() | true == person.getCholestPb() | person.getImc().equals(Person.Imc.no) | person.getParentPb().equals(Person.ParentPb.yes)) {
-
                                 TextView text2 = findViewById(R.id.textView13);
-                                text2.setText("you need to see a keep a healthy life style");
-
+                                text2.setText("You need to see a keep a healthy life style");
                             }
                         } else {
                             if(person.getImc().equals(Person.Imc.no) | (person.getImc().equals(Person.Imc.IDontKnow))){
@@ -69,11 +80,10 @@ public class Results extends AppCompatActivity {
                     } else {
 
                         if (false == person.getDiab() & false == person.getCardiacDisease() & false == person.getHypertension() & false == person.getCholestPb()) {
-
                             TextView text2 = findViewById(R.id.textView12);
                             text2.setText("you need to see a keep a healthy life style");
-
-                        } else {
+                        }
+                        else {
 
                             TextView text2 = findViewById(R.id.textView13);
                             text2.setText("you are good");
@@ -81,24 +91,22 @@ public class Results extends AppCompatActivity {
                         }
                     }
                 }
-                    if(person.getImc().equals(Person.Imc.yes)){
-                        TextView textImc = findViewById(R.id.textView14);
-                        textImc.setVisibility(View.INVISIBLE);
-                        Button imc = findViewById(R.id.button2);
-                        imc.setVisibility(View.INVISIBLE);
+                if(person.getImc().equals(Person.Imc.yes)){
+                    TextView textImc = findViewById(R.id.textView14);
+                    textImc.setVisibility(View.INVISIBLE);
+                    Button imc = findViewById(R.id.button2);
+                    imc.setVisibility(View.INVISIBLE);
+                }
+                if(person.getParentPb().equals(Person.ParentPb.maybe)){
+                    TextView textImc = findViewById(R.id.textView14);
+                    textImc.setText("You should talk with your family about heart diseases");
+                }
+                if(person.getImc().equals(Person.Imc.no) | (person.getImc().equals(Person.Imc.IDontKnow))){
+                    TextView text2 = findViewById(R.id.textView14);
+                    text2.setText("you need to check your imc ");
 
 
-                    }
-                    if(person.getParentPb().equals(Person.ParentPb.maybe)){
-                        TextView textImc = findViewById(R.id.textView14);
-                        textImc.setText("Talk with your family");
-                    }
-                    if(person.getImc().equals(Person.Imc.no) | (person.getImc().equals(Person.Imc.IDontKnow))){
-                        TextView text2 = findViewById(R.id.textView14);
-                        text2.setText("you need to check your imc ");
-
-
-                    }
+                }*/
 
 
             }
